374c17035a72129dd83fdb1fd1d379b377420c0f
e <hejiuwei@163.com>
2019-04-25 17:33:51 +0800

    爆发式通信对告警统计

--- data-extension-job-schedule/src/main/java/cn/csg/jobschedule/job/CommunicationAlarmJob.java
+++ data-extension-job-schedule/src/main/java/cn/csg/jobschedule/job/CommunicationAlarmJob.java
@@ -1,9 +1,11 @@
 package cn.csg.jobschedule.job;
 
 import cn.csg.jobschedule.service.MetadataService;
-import org.apache.log4j.Logger;
+import com.alibaba.fastjson.JSONObject;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
@@ -16,18 +18,185 @@ import java.util.Date;
  */
 public class CommunicationAlarmJob extends QuartzJobBean {
 
-    private Logger logger = Logger.getLogger(this.getClass());
+    private final static Logger logger = LoggerFactory.getLogger(CommunicationAlarmJob.class);
 
     @Autowired
     public MetadataService metadataService;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("----------start-------------");
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        logger.error(simpleDateFormat.format(new Date())+":爆发式通信对告警处理！");
-        String queryStr = "{\"aggs\":{\"corpIdCount\":{\"aggs\":{\"srcIpCount\":{\"aggs\":{\"destIpCount\":{\"aggs\":{\"destPortCount\":{\"terms\":{\"field\":\"destPort\"}}},\"terms\":{\"field\":\"destIp\"}}},\"terms\":{\"field\":\"srcIp\"}}},\"terms\":{\"field\":\"corpId\"}}},\"query\":{\"range\":{\"createTime\":{\"gte\":\"2019-04-24T15:00:00+08:00\",\"lt\":\"2019-04-24T16:00:00+08:00\"}}},\"size\":0}";
-        System.out.println(metadataService.getMetadataByHttp(queryStr));
-        System.out.println("----------end-------------");
+        //srcIp在t分钟内发起n次访问 start
+        String srcIpSumQueryStr = getSrcIpSumQueryStr();
+        JSONObject srcIpSumJson = metadataService.getResultByHttp(srcIpSumQueryStr);
+        metadataService.handleSrcIpSumData(srcIpSumJson);
+        //srcIp在t分钟内发起n次访问 end
+
+        //srcIp在t分钟内访问了n个destIp start
+        String srcIpAndDestIpCountQueryStr = getSrcIpAndDestIpCountQueryStr();
+        JSONObject srcIpAndDestIpCountJson = metadataService.getResultByHttp(srcIpAndDestIpCountQueryStr);
+        metadataService.handleSrcIpAndDestIpCountData(srcIpAndDestIpCountJson);
+        //srcIp在t分钟内访问了n个destIp end
+
+        //srcIp在t分钟内访问destIp的n个端口 start
+        String srcIpAndDestPortCountQueryStr = getSrcIpAndDestPortCountQueryStr();
+        JSONObject srcIpAndDestPortCountJson = metadataService.getResultByHttp(srcIpAndDestPortCountQueryStr);
+        metadataService.handleSrcIpAndDestPortCountData(srcIpAndDestPortCountJson);
+        //srcIp在t分钟内访问destIp的n个端口 end
+    }
+
+    /**
+     * (1)组装 srcIp在t分钟内发起n次访问 统计条件
+     * @return
+     */
+    private String getSrcIpSumQueryStr(){
+        String queryStr = "{\n" +
+                "    \"aggs\": {\n" +
+                "        \"corpIdCount\": {\n" +
+                "            \"aggs\": {\n" +
+                "                \"srcIpCount\": {\n" +
+                "                    \"aggs\": {\n" +
+                "                        \"doubleCountSum\": {\n" +
+                "                            \"sum\": {\n" +
+                "                                \"field\": \"doubleCount\"\n" +
+                "                            }\n" +
+                "                        },\n" +
+                "                        \"doubleCount_filter\": {\n" +
+                "                            \"bucket_selector\": {\n" +
+                "                                \"buckets_path\": {\n" +
+                "                                    \"filterKey\": \"doubleCountSum\"\n" +
+                "                                },\n" +
+                "                                \"script\": \"params.filterKey >= 10\"\n" +
+                "                            }\n" +
+                "                        }\n" +
+                "                    },\n" +
+                "                    \"terms\": {\n" +
+                "                        \"field\": \"srcIp\"\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"terms\": {\n" +
+                "                \"field\": \"corpId\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"query\": {\n" +
+                "        \"range\": {\n" +
+                "            \"createTime\": {\n" +
+                "                \"gte\": \"2019-04-24T15:00:00+08:00\",\n" +
+                "                \"lt\": \"2019-04-24T16:00:00+08:00\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"size\": 0\n" +
+                "}";
+        return queryStr;
+    }
+
+    /**
+     * (2)组装 srcIp在t分钟内访问了n个destIp 统计条件
+     * @return
+     */
+    private String getSrcIpAndDestIpCountQueryStr(){
+        String queryStr = "{\n" +
+                "    \"aggs\": {\n" +
+                "        \"corpIdCount\": {\n" +
+                "            \"aggs\": {\n" +
+                "                \"srcIpCount\": {\n" +
+                "                    \"aggs\": {\n" +
+                "                        \"destIpCount\": {\n" +
+                "                            \"terms\": {\n" +
+                "                                \"field\": \"destIp\"\n" +
+                "                            }\n" +
+                "                        },\n" +
+                "                        \"having\": {\n" +
+                "                            \"bucket_selector\": {\n" +
+                "                                \"buckets_path\": {\n" +
+                "                                    \"srcIpCount\": \"_count\"\n" +
+                "                                },\n" +
+                "                                \"script\": {\n" +
+                "                                    \"source\": \"params.srcIpCount > 0\"\n" +
+                "                                }\n" +
+                "                            }\n" +
+                "                        }\n" +
+                "                    },\n" +
+                "                    \"terms\": {\n" +
+                "                        \"field\": \"srcIp\"\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"terms\": {\n" +
+                "                \"field\": \"corpId\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"query\": {\n" +
+                "        \"range\": {\n" +
+                "            \"createTime\": {\n" +
+                "                \"gte\": \"2019-04-24T15:00:00+08:00\",\n" +
+                "                \"lt\": \"2019-04-24T16:00:00+08:00\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"size\": 0\n" +
+                "}";
+        return queryStr;
+    }
+
+    /**
+     * (3)组装 srcIp在t分钟内访问destIp的n个端口 查询条件
+     * @return
+     */
+    private String getSrcIpAndDestPortCountQueryStr(){
+        String queryStr = "{\n" +
+                "    \"aggs\": {\n" +
+                "        \"corpIdCount\": {\n" +
+                "            \"aggs\": {\n" +
+                "                \"srcIpCount\": {\n" +
+                "                    \"aggs\": {\n" +
+                "                        \"destIpCount\": {\n" +
+                "                            \"aggs\": {\n" +
+                "                                \"destPortCount\": {\n" +
+                "                                    \"terms\": {\n" +
+                "                                        \"field\": \"destPort\"\n" +
+                "                                    }\n" +
+                "                                },\n" +
+                "                                \"having\": {\n" +
+                "                                    \"bucket_selector\": {\n" +
+                "                                        \"buckets_path\": {\n" +
+                "                                            \"destIpCount\": \"_count\"\n" +
+                "                                        },\n" +
+                "                                        \"script\": {\n" +
+                "                                            \"source\": \"params.destIpCount > 0\"\n" +
+                "                                        }\n" +
+                "                                    }\n" +
+                "                                }\n" +
+                "                            },\n" +
+                "                            \"terms\": {\n" +
+                "                                \"field\": \"destIp\"\n" +
+                "                            }\n" +
+                "                        }\n" +
+                "                    },\n" +
+                "                    \"terms\": {\n" +
+                "                        \"field\": \"srcIp\"\n" +
+                "                    }\n" +
+                "                }\n" +
+                "            },\n" +
+                "            \"terms\": {\n" +
+                "                \"field\": \"corpId\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"query\": {\n" +
+                "        \"range\": {\n" +
+                "            \"createTime\": {\n" +
+                "                \"gte\": \"2019-04-24T15:00:00+08:00\",\n" +
+                "                \"lt\": \"2019-04-24T16:00:00+08:00\"\n" +
+                "            }\n" +
+                "        }\n" +
+                "    },\n" +
+                "    \"size\": 0\n" +
+                "}";
+
+        return queryStr;
     }
 }
--- data-extension-job-schedule/src/main/java/cn/csg/jobschedule/service/MetadataService.java
+++ data-extension-job-schedule/src/main/java/cn/csg/jobschedule/service/MetadataService.java
@@ -5,12 +5,20 @@ import com.alibaba.fastjson.JSONObject;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class MetadataService {
+
+    private final static Logger logger = LoggerFactory.getLogger(MetadataService.class);
 
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
@@ -18,9 +26,135 @@ public class MetadataService {
     @Autowired
     private ElasticsearchDao elasticsearchDao;
 
-    public JSONObject getMetadataByHttp(String queryStr){
+    /**
+     * 前往ES集群获取数据
+     * @param queryStr
+     * @return
+     */
+    public JSONObject getResultByHttp(String queryStr){
         return elasticsearchDao.requestMyTest(JSONObject.parseObject(queryStr));
     }
+
+    //srcIp在t分钟内发起n次访问
+    public void handleSrcIpSumData(JSONObject jsonObj){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map aggregationsMap = (Map)jsonObj.get("aggregations");
+                    Map corpIdCountMap = (Map)aggregationsMap.get("corpIdCount");
+                    List<Map> buckets = (List)corpIdCountMap.get("buckets");
+                    if(buckets != null && buckets.size() > 0){
+                        for(Map corpIdGroupMap : buckets){
+                            String conrpId = corpIdGroupMap.get("key")+"";
+                            Map srcIpCountMap = (Map)corpIdGroupMap.get("srcIpCount");
+                            List<Map> sumResultBuckets = (List)srcIpCountMap.get("buckets");
+                            if(sumResultBuckets != null && sumResultBuckets.size() > 0){
+                                for(Map sumResultMap : sumResultBuckets){
+                                    String srcIp = sumResultMap.get("key")+"";
+                                    Map doubleCountSumMap = (Map)sumResultMap.get("doubleCountSum");
+                                    Double countData = (Double) doubleCountSumMap.get("value");
+                            System.out.println("conrpId="+conrpId+";srcIp="+srcIp+";countData="+countData);
+                                    //TODO srcIp在t分钟内发起n次访问 保存到ES
+                                }
+                            }
+                        }
+                    }
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                    logger.error("处理 srcIp在t分钟内发起n次访问 异常:"+e.getMessage());
+                }
+
+            }
+        }).start();
+    }
+
+    //srcIp在t分钟内访问了n个destIp
+    public void handleSrcIpAndDestIpCountData(JSONObject jsonObj) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map aggregationsMap = (Map) jsonObj.get("aggregations");
+                    Map corpIdCountMap = (Map) aggregationsMap.get("corpIdCount");
+                    List<Map> buckets = (List) corpIdCountMap.get("buckets");
+                    if (buckets != null && buckets.size() > 0) {
+                        for (Map corpIdGroupMap : buckets) {
+                            String conrpId = corpIdGroupMap.get("key") + "";
+                            Map srcIpCountMap = (Map) corpIdGroupMap.get("srcIpCount");
+                            List<Map> srcIpCountResultList = (List) srcIpCountMap.get("buckets");
+                            if (srcIpCountResultList != null && srcIpCountResultList.size() > 0) {
+                                for (Map srcIpCountResultMap : srcIpCountResultList) {
+                                    String srcIp = srcIpCountResultMap.get("key") + "";
+                                    Integer countData = (Integer) srcIpCountResultMap.get("doc_count");
+                                    Map destIpCountMap = (Map) srcIpCountResultMap.get("destIpCount");
+                                    List<Map> destIpCountList = (List) destIpCountMap.get("buckets");
+                                    List destIpList = new ArrayList();
+                                    for (Map destIpMap : destIpCountList) {
+                                        destIpList.add(destIpMap.get("key"));
+                                    }
+                            System.out.println("conrpId=" + conrpId + ";srcIp=" + srcIp + ";countData=" + countData + ";destIpList=" + destIpList);
+                                    //TODO srcIp在t分钟内访问了n个destIp 入库ES
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("处理 srcIp在t分钟内访问了n个destIp 异常:" + e.getMessage());
+                }
+
+            }
+        }).start();
+    }
+
+    //srcIp在t分钟内访问destIp的n个端口
+    public void handleSrcIpAndDestPortCountData(JSONObject jsonObj) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map aggregationsMap = (Map) jsonObj.get("aggregations");
+                    Map corpIdCountMap = (Map) aggregationsMap.get("corpIdCount");
+                    List<Map> buckets = (List) corpIdCountMap.get("buckets");
+                    if (buckets != null && buckets.size() > 0) {
+                        for (Map corpIdGroupMap : buckets) {
+                            String conrpId = corpIdGroupMap.get("key") + "";
+                            Map srcIpCountMap = (Map) corpIdGroupMap.get("srcIpCount");
+                            List<Map> srcIpCountResultList = (List) srcIpCountMap.get("buckets");
+                            if (srcIpCountResultList != null && srcIpCountResultList.size() > 0) {
+                                for (Map srcIpCountResultMap : srcIpCountResultList) {
+                                    String srcIp = srcIpCountResultMap.get("key") + "";
+                                    Map destIpCountMap = (Map) srcIpCountResultMap.get("destIpCount");
+                                    List<Map> destIpCountBuckets = (List) destIpCountMap.get("buckets");
+                                    if(destIpCountBuckets != null && destIpCountBuckets.size() > 0){
+                                        for(Map destIpResultMap : destIpCountBuckets){
+                                            String destIp = destIpResultMap.get("key")+"";
+                                            Integer countData = (Integer)destIpResultMap.get("doc_count");
+                                            Map destPortCount = (Map)destIpResultMap.get("destPortCount");
+                                            List<Map> destPortBuckets = (List)destPortCount.get("buckets");
+                                            List destPortList = new ArrayList();
+                                            for(Map destPortMap : destPortBuckets){
+                                                destPortList.add(destPortMap.get("key"));
+                                            }
+                                            System.out.println("conrpId="+conrpId+";srcIp="+srcIp+";destIp="+destIp+";countData="+countData+";destPortList="+destPortList);
+                                            //TODO srcIp在t分钟内访问destIp的n个端口 入库ES
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    logger.error("处理 srcIp在t分钟内访问destIp的n个端口 异常:" + e.getMessage());
+                }
+            }
+        }).start();
+    }
+
 
     public String getMetadata(){
 //        Client transportClient = elasticsearchTemplate.getClient();
