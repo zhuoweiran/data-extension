36400bd04d51cecfa0f0744bc218dc5d1f11a691
e <hejiuwei@163.com>
2019-04-26 16:53:18 +0800

    爆发式通信对告警统计,ES入库连接参数更改为通过配置获取

--- data-extension-job-schedule/src/main/java/cn/csg/jobschedule/DataExtensionJobScheduleApplication.java
+++ data-extension-job-schedule/src/main/java/cn/csg/jobschedule/DataExtensionJobScheduleApplication.java
@@ -14,8 +14,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableFeignClients
 public class DataExtensionJobScheduleApplication extends SpringBootServletInitializer {
-    @Autowired
-    private ApplicationProperties applicationProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(DataExtensionJobScheduleApplication.class,args);
