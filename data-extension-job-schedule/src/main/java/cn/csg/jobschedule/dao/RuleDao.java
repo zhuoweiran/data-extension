af965bf84d915c4acd7468e77626bc28a304b86d
hanwl <hanwl@123.com>
2019-04-26 18:20:34 +0800

    修改之和返回值格式

--- data-extension-job-schedule/src/main/java/cn/csg/jobschedule/dao/RuleDao.java
+++ data-extension-job-schedule/src/main/java/cn/csg/jobschedule/dao/RuleDao.java
@@ -2,7 +2,9 @@ package cn.csg.jobschedule.dao;
 
 import cn.csg.common.vo.RuleVo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RuleDao extends JpaRepository<RuleVo,String> {
-    RuleVo findFirstByKey(String key);
+    @Query(value = "select id from tb_rule where rule_key=?1 and rule=?2" , nativeQuery = true)
+    String findFirstByKeyAndRule(String key,String rule);
 }
