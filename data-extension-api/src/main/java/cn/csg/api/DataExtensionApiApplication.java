9e1220372d0a28b76b36d87a26d8776c16e88543
hanwl <hanwl@123.com>
2019-06-26 15:45:27 +0800

    去掉一些编译错误问题

--- data-extension-api/src/main/java/cn/csg/api/DataExtensionApiApplication.java
+++ data-extension-api/src/main/java/cn/csg/api/DataExtensionApiApplication.java
@@ -1,10 +1,8 @@
 package cn.csg.api;
 
-import cn.csg.common.vo.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 
 @SpringBootApplication
