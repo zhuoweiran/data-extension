9408d714c4cfc4cef92f064d21238a372db2bed5
hanwl <hanwl@123.com>
2019-06-27 16:57:47 +0800

    添加javadoc

--- data-extension-msg-producer/src/main/java/cn/csg/msg/producer/controller/TestController.java
+++ data-extension-msg-producer/src/main/java/cn/csg/msg/producer/controller/TestController.java
@@ -16,12 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 
+/**
+ * <p>类{@code TestController} 用来测试一些RESTFul功能
+ *
+ * @author Alex Han
+ * @since 1.0
+ * @version 1.2
+ */
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @Autowired
     private JedisResourcePool jedisResourcePool;
 
+    /**
+     * 初始化redis资产的guid存储于deviceGuidSet
+     * <p>HttpMethod:Get</p>
+     * <p>URL:/msg-producer/test/init/device</p>
+     *
+     */
     @ApiOperation(value ="初始化资产",httpMethod = "GET")
     @GetMapping("/init/device")
     public void initJedisDevice(){
@@ -34,6 +47,13 @@ public class TestController {
         System.out.println("add all over");
         jedis.close();
     }
+
+    /**
+     * 初始化redis未知资产的guid存储于tmpDeviceGuidSet
+     * <p>HttpMethod:Get</p>
+     * <p>URL:/msg-producer/test/init/tmpDevice</p>
+     *
+     */
     @ApiOperation(value ="初始化未知资产",httpMethod = "GET")
     @GetMapping("/init/tmpDevice")
     public void initJedisTmpDevice(){
@@ -46,6 +66,16 @@ public class TestController {
         System.out.println("add all over");
         jedis.close();
     }
+
+    /**
+     * 根据传入的Array初始化一个名为setName的redis资产
+     * <p>HttpMethod:Post</p>
+     * <p>URL:/msg-producer/test/init/selfDevice</p>
+     *
+     * @param setName redis set 名称
+     * @param array 资产json array
+     * @return {@code ResultData}
+     */
     @ApiOperation(value ="初始化自己的资产",httpMethod = "POST")
     @PostMapping("/init/selfDevice")
     public ResultData initJedisSelfDevice(String setName,String array){
@@ -57,6 +87,14 @@ public class TestController {
         jedis.close();
         return new ResultData<>(null, ResultStatus.initStatus(StatusEnum.SUCCESS));
     }
+
+    /**
+     * 测试redis中资产反序列化为Object
+     * <p>HttpMethod:Post</p>
+     * <p>URL:/msg-producer/test/init/testJedisToObj</p>
+     *
+     * @return {@code ResultData<DeviceRedis>}
+     */
     @ApiOperation(value ="测试redis资产序列化为Object",httpMethod = "GET")
     @GetMapping("/init/testJedisToObj")
     public ResultData testJedisToObj(){
