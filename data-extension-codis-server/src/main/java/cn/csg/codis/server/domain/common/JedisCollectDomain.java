122c32a7f6f3ad1416f4fbd431bde44f66439ad3
hanwl <hanwl@123.com>
2019-07-15 16:21:49 +0800

    添加javadoc

--- data-extension-codis-server/src/main/java/cn/csg/codis/server/domain/common/JedisCollectDomain.java
+++ data-extension-codis-server/src/main/java/cn/csg/codis/server/domain/common/JedisCollectDomain.java
@@ -1,4 +1,11 @@
 package cn.csg.codis.server.domain.common;
 
+/**
+ * 类{@code JedisCollectDomain} 所有redis存储数据的父类
+ *
+ * @author Alex Han
+ * @since 1.2
+ * @version 1.2
+ */
 public class JedisCollectDomain {
 }
